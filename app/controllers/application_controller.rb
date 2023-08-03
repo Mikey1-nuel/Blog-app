a19250569af71fde5b4e9dfd041aed7b8706caf8
Emmanuel <emmanuelnwoye12@gmail.com>
2023-08-03 10:16:37 +0100

    Fix linter errors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-   protect_from_forgery with: :exception
+  protect_from_forgery with: :exception
 
   before_action :update_allowed_parameters, if: :devise_controller?
 
@@ -10,5 +10,5 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update) do |u|
       u.permit(:name, :email, :bio, :password, :current_password)
     end
-  end 
+  end
 end
