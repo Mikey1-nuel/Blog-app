a2c54944b94438761d4f947ad9fe849330f27158
Emmanuel <emmanuelnwoye12@gmail.com>
2023-08-03 14:58:33 +0100

    Update file

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -18,6 +18,7 @@ class CommentsController < ApplicationController
       render :new
     end
   end
+  
 
   private
 
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  load_and_authorize_resource except: %i[index show]
+
   def index
     @user = User.find(params[:user_id])
     @posts = Post.includes(comments: [:author]).where(author_id: params[:user_id])
@@ -23,6 +25,8 @@ class PostsController < ApplicationController
       render :new
     end
   end
+
+  
 
   private
 
